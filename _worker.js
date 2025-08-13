61169b291fa1e1e75ce0221e1da9e4618f13bd53
Mikasa33 <69848344+mikasa33-creator@users.noreply.github.com>
2025-08-14 05:26:48 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,8 +3,8 @@ import { connect } from "cloudflare:sockets";
 // import { Buffer } from "node:buffer";
 
 // Variables
-const rootDomain = "aeckerman33.workers.dev"; // Ganti dengan domain utama kalian
-const serviceName = "mikasa"; // Ganti dengan nama workers kalian
+const rootDomain = "com"; // Ganti dengan domain utama kalian
+const serviceName = "mikasa3321"; // Ganti dengan nama workers kalian
 const apiKey = ""; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
 const apiEmail = ""; // Ganti dengan email yang kalian gunakan
 const accountID = ""; // Ganti dengan Account ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
